126dcc89df93654fc4559f5ae618fe6c59a8f8c7
Keiaa <confinedrose@duck.com>
2025-11-14 01:40:06 +0800

    feat: create main app layout with navbar and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup></script>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <RouterLink class="navbar-item is-size-4 has-text-weight-bold" to="/">
+          SeatVue
+        </RouterLink>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <RouterLink class="navbar-item" to="/">Home</RouterLink>
+          <RouterLink class="navbar-item" to="/manage">Manage</RouterLink>
+          <RouterLink class="navbar-item" to="/about">About</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Add a class to make active links bold */
+.router-link-active {
+  font-weight: bold;
+}
+</style>
