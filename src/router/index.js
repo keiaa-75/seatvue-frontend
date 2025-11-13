e465dfadfbb4b61fada3f79aa9c7f2d2a1cdad3b
Keiaa <confinedrose@duck.com>
2025-11-14 01:38:27 +0800

    feat: import bulma and add routes for main views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ManageView from '../views/ManageView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/manage',
+      name: 'manage',
+      component: ManageView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    }
+  ]
 })
 
 export default router
