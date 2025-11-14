c8aef0fc8598ffdec69ecf033d60d9418fc16b9e
Keiaa <confinedrose@duck.com>
2025-11-14 12:43:47 +0800

    feat: add all child routes for section management

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import ManageView from '../views/ManageView.vue'
 import AboutView from '../views/AboutView.vue'
 import SectionDashboard from '../views/SectionDashboard.vue'
 
+import NewAssignmentView from '../views/NewAssignmentView.vue'
+import ManageAssignmentsView from '../views/ManageAssignmentsView.vue'
+import ManageStudentsView from '../views/ManageStudentsView.vue'
+import ImportStudentsView from '../views/ImportStudentsView.vue'
+import EditSectionView from '../views/EditSectionView.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,6 +28,32 @@ const router = createRouter({
           path: ':sectionId',
           name: 'section-dashboard',
           component: SectionDashboard
+        },
+
+        {
+          path: ':sectionId/assignments/new',
+          name: 'new-assignment',
+          component: NewAssignmentView
+        },
+        {
+          path: ':sectionId/assignments',
+          name: 'manage-assignments',
+          component: ManageAssignmentsView
+        },
+        {
+          path: ':sectionId/students',
+          name: 'manage-students',
+          component: ManageStudentsView
+        },
+        {
+          path: ':sectionId/students/import',
+          name: 'import-students',
+          component: ImportStudentsView
+        },
+        {
+          path: ':sectionId/edit',
+          name: 'edit-section',
+          component: EditSectionView
         }
       ]
     },
---
+++ src/views/ManageStudentsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <h1 class="title">Manage Students</h1>
+      <p>This is where the list of students will go.</p>
+    </div>
+  </div>
+</template>
