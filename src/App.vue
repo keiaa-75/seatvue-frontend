840b24db18699166a7ccdbc40de96779815dd424
Keiaa <confinedrose@duck.com>
2025-11-14 12:36:39 +0800

    feat: add fontawesome icons

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,17 @@ import { RouterLink, RouterView } from 'vue-router'
 
   <nav class="bottom-nav" role="navigation" aria-label="bottom navigation">
     <RouterLink class="bottom-nav-item" to="/">
+      <i class="fas fa-home bottom-nav-icon"></i>
       <span class="bottom-nav-text">Home</span>
     </RouterLink>
     
     <RouterLink class="bottom-nav-item" to="/manage">
+      <i class="fas fa-tasks bottom-nav-icon"></i>
       <span class="bottom-nav-text">Manage</span>
     </RouterLink>
     
     <RouterLink class="bottom-nav-item" to="/about">
+      <i class="fas fa-info-circle bottom-nav-icon"></i>
       <span class="bottom-nav-text">About</span>
     </RouterLink>
   </nav>
