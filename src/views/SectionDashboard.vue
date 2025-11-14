f0050a9329503cfd004ab1953907b9a43a19ca13
Keiaa <confinedrose@duck.com>
2025-11-14 12:39:09 +0800

    style: attempt to restyle the section dashboard for a mobile-first approach

--- src/views/SectionDashboard.vue
+++ src/views/SectionDashboard.vue
@@ -1,69 +1,56 @@
 <script setup>
 import { RouterLink, useRoute } from 'vue-router'
 
-// Get the current route object to access URL parameters
 const route = useRoute()
 const sectionId = route.params.sectionId
 </script>
 
 <template>
-  <div>
-    <h2 class="title is-4">Manage Section (ID: {{ sectionId }})</h2>
-    <p class="subtitle is-6">What would you like to do?</p>
+  <div class="section">
+    <h1 class="title is-4">What to do?</h1>
+    <p class="subtitle is-6">Section ID: {{ sectionId }}</p>
 
-    <div class="columns is-multiline">
-      <div class="column is-one-third">
-        <div class="box has-text-centered">
-          <span class="icon is-large has-text-primary">
-            <i class="fas fa-users fa-2x"></i>
-          </span>
-          <h3 class="title is-5 mt-3">Manage Students</h3>
-          <p class="mb-4">Add, edit, or import students for this section.</p>
-          <RouterLink :to="`/manage/${sectionId}/students`" class="button is-primary is-fullwidth">
-            Go
-          </RouterLink>
-        </div>
-      </div>
+    <div class="buttons is-vcentered is-fullwidth">
+      <RouterLink :to="`/manage/${sectionId}/assignments/new`" class="button is-success is-fullwidth is-medium mb-3">
+        <span class="icon is-left">
+          <i class="fas fa-plus"></i>
+        </span>
+        <span>New Assignment</span>
+      </RouterLink>
 
-      <div class="column is-one-third">
-        <div class="box has-text-centered">
-          <span class="icon is-large has-text-info">
-            <i class="fas fa-chair fa-2x"></i>
-          </span>
-          <h3 class="title is-5 mt-3">Manage Assignments</h3>
-          <p class="mb-4">Assign seats to students for a classroom layout.</p>
-          <RouterLink :to="`/manage/${sectionId}/assignments`" class="button is-info is-fullwidth">
-            Go
-          </RouterLink>
-        </div>
-      </div>
+      <RouterLink :to="`/manage/${sectionId}/assignments`" class="button is-info is-fullwidth is-medium mb-3">
+        <span class="icon is-left">
+          <i class="fas fa-chair"></i>
+        </span>
+        <span>Manage Assignments</span>
+      </RouterLink>
 
-      <div class="column is-one-third">
-        <div class="box has-text-centered">
-          <span class="icon is-large has-text-success">
-            <i class="fas fa-edit fa-2x"></i>
-          </span>
-          <h3 class="title is-5 mt-3">Edit Section Details</h3>
-          <p class="mb-4">Update the name, grade level, or strand for this section.</p>
-          <RouterLink :to="`/manage/${sectionId}/edit`" class="button is-success is-fullwidth">
-            Go
-          </RouterLink>
-        </div>
-      </div>
+      <RouterLink :to="`/manage/${sectionId}/students`" class="button is-primary is-fullwidth is-medium mb-3">
+        <span class="icon is-left">
+          <i class="fas fa-users"></i>
+        </span>
+        <span>Manage Students</span>
+      </RouterLink>
+
+      <RouterLink :to="`/manage/${sectionId}/students/import`" class="button is-link is-fullwidth is-medium mb-3">
+        <span class="icon is-left">
+          <i class="fas fa-file-upload"></i>
+        </span>
+        <span>Import Students</span>
+      </RouterLink>
+
+      <RouterLink :to="`/manage/${sectionId}/edit`" class="button is-warning is-fullwidth is-medium mb-3">
+        <span class="icon is-left">
+          <i class="fas fa-edit"></i>
+        </span>
+        <span>Manage Section</span>
+      </RouterLink>
     </div>
   </div>
 </template>
 
 <style scoped>
-.icon i {
-  font-size: 2.5rem;
-}
-.box {
-  display: flex;
+.buttons.is-vcentered {
   flex-direction: column;
-  height: 100%;
-}
-.box .button {
-  margin-top: auto;
 }
 </style>
