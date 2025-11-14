8faaed1c8df523096949769a042fef64df5474b5
Keiaa <confinedrose@duck.com>
2025-11-14 12:21:42 +0800

    feat: replace top nav with mobile-first bottom nav

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,21 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
-        <RouterLink class="navbar-item is-size-4 has-text-weight-bold" to="/">
-          SeatVue
-        </RouterLink>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <RouterLink class="navbar-item" to="/">Home</RouterLink>
-          <RouterLink class="navbar-item" to="/manage">Manage</RouterLink>
-          <RouterLink class="navbar-item" to="/about">About</RouterLink>
-        </div>
-      </div>
-    </div>
-  </nav>
-
   <main>
     <RouterView />
   </main>
-</template>
 
-<style scoped>
-/* Add a class to make active links bold */
-.router-link-active {
-  font-weight: bold;
-}
-</style>
+  <nav class="bottom-nav" role="navigation" aria-label="bottom navigation">
+    <RouterLink class="bottom-nav-item" to="/">
+      <span class="bottom-nav-text">Home</span>
+    </RouterLink>
+    
+    <RouterLink class="bottom-nav-item" to="/manage">
+      <span class="bottom-nav-text">Manage</span>
+    </RouterLink>
+    
+    <RouterLink class="bottom-nav-item" to="/about">
+      <span class="bottom-nav-text">About</span>
+    </RouterLink>
+  </nav>
+</template>
